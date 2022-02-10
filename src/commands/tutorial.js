782c68cbb994e7adb6e3575b4d51842353ab987d
duvu <hoaivubk@gmail.com>
2022-02-10 17:48:26 +0700

    updated to send DM

--- src/commands/tutorial.js
+++ src/commands/tutorial.js
@@ -1,13 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu, WebhookClient } = require('discord.js');
 const { bold, italic, strikethrough, underscore, spoiler, quote, blockQuote } = require('@discordjs/builders');
+const { webhookId, webhookToken } = require('../../config.json');
+const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('tutorial')
-        .setDefaultPermission(true)
-        .setDescription('Start the tutorial of the bot!'),
+        .setDescription('Start the tutorial of the bot!')
+        .addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
     async execute(interaction) {
+
         const italicNotes = italic('You will not receive any rewards for replaying the tutorial.');
         const tutorialMessage = new MessageEmbed()
             .setColor('#0099ff')
@@ -27,6 +30,26 @@ module.exports = {
             .setDescription('Thanks so much!\n\n Start exploring with your Clockie bot using ```r!expedition!```')
             .setImage('https://i8.ae/QpVMA')
             .setTimestamp();
+
+        const msgExpedition = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Checking Expedition Status')
+            .setDescription('```Expedition status```')
+
+        const msgVisitAcademy = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Visit Academy')
+            .setDescription('```Visit Academy contents```')
+
+        const msgStartExpedition = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Start Expedition')
+            .setDescription('```Start Expedition Contents```')
+
+        const msgEventsTimeEnd = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Event TimeEnd')
+            .setDescription('```Event TimeEnd```')
 
         const btnReplay = new MessageActionRow()
             .addComponents(
@@ -52,6 +75,63 @@ module.exports = {
         const btnExpedition = new MessageActionRow().addComponents(
             new MessageButton().setCustomId('r_expedition').setLabel('r!expedition').setStyle('PRIMARY')
         );
+
+        const btnVisitAcademy = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId('visit_academy').setLabel('Visit Academy').setStyle('PRIMARY')
+        );
+
+        const btnExpeditionInventoryArtifact = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('start_expedition')
+                    .setLabel('Start Expedition')
+                    .setStyle('PRIMARY'),
+            ).addComponents(
+                new MessageButton()
+                    .setCustomId('clockie_inventory')
+                    .setLabel('Clockie Inventory')
+                    .setStyle('PRIMARY'),
+            ).addComponents(
+                new MessageButton()
+                    .setCustomId('artifacts')
+                    .setLabel('Artifacts')
+                    .setStyle('PRIMARY'),
+            );
+
+        const btnExit = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId('exit').setLabel('Exit').setStyle('DANGER')
+        );
+
+        const menuSelect = new MessageActionRow()
+            .addComponents(
+                new MessageSelectMenu()
+                    .setCustomId('select')
+                    .setPlaceholder('Nothing selected')
+                    .addOptions([
+                        {
+                            label: 'Time End',
+                            description: '',
+                            value: 'time_end',
+                        },
+                        {
+                            label: 'The syphon COMING SOON',
+                            description: '',
+                            value: 'the_syphon_coming_soon',
+                        },
+                        {
+                            label: 'Paradox Forge COMING SOON',
+                            description: '',
+                            value: 'paradox_forge_coming_soon',
+                        },
+                        {
+                            label: 'Planet Burner COMING SOON',
+                            description: '',
+                            value: 'planet_burner_coming_soon',
+                        },
+                    ]),
+            );
+
+
         // -- button
         const boldTutMess = bold('Tutorial [' + interaction.user.tag + ']');
         const btnFilter = i => i.customId === 'replay_tutorial';
@@ -62,7 +142,32 @@ module.exports = {
             } else if (i.customId === 'accept') {
                 await i.update({ content: boldTutMess, embeds: [msgAccepted], components: [btnExpedition] });
             } else if (i.customId === 'r_expedition') {
+                await i.update({content: boldTutMess, embeds: [msgExpedition], components: [btnVisitAcademy]});
+            } else if (i.customId === 'visit_academy') {
+                await i.update({content: boldTutMess, embeds: [msgVisitAcademy], components: [btnExpeditionInventoryArtifact]});
+            } else if (i.customId === 'start_expedition') {
+                await i.update({content: boldTutMess, embeds: [msgStartExpedition], components: [menuSelect, btnExit]});
+            } else if (i.customId === 'select') {
+                if (i.values[0] === 'time_end') {
+                    await i.update({ content: 'TimeEnd was selected!', components: [] });
 
+                    const time_end_selected = {
+                        content: 'User xyz has began its adventure into Time\' End',
+                        avatarURL: 'https://i8.ae/OuCQM',
+                        embeds: [msgEventsTimeEnd],
+                    };
+                    await webhookClient.send(time_end_selected);
+
+                    const user = interaction.options.getUser('target');
+                    if (user) {
+                        user.send('TimeEnd was selected!!!');
+                    } else {
+                        interaction.user.send('TimeEnd was selected!!!')
+                    }
+
+                }
+            } else {
+                console.log(i.values)
             }
         });
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('../config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+
+    ],
+    partials: [
+        'CHANNEL', // Required to receive DMs
+    ] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
@@ -25,10 +38,11 @@ for (const file of eventFiles) {
 }
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand() && !interaction.isButton()) return;
+    if (!interaction.isCommand() && !interaction.isButton() && !interaction.isSelectMenu()) return;
 
     const command = client.commands.get(interaction.commandName);
-    console.log('Interaction#', interaction);
+
+    // console.log('Interaction#', interaction);
     if (!command) return;
 
     try {
